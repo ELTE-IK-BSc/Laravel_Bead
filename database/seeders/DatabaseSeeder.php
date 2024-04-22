d81aeb7ce80b07b25607ea5029899179b447507c
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-22 09:31:05 +0200

    Add admin boolean

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'admin' => true,
         ]);
     }
 }
