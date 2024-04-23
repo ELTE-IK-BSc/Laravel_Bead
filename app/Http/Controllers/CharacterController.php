b24f64b321dcc830045bd51387238670f63a3ec0
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-24 00:15:45 +0200

    Add index view

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class CharacterController extends Controller
 {
@@ -11,7 +13,8 @@ class CharacterController extends Controller
      */
     public function index()
     {
-        //
+        $characters = Auth::user()->characters()->get();
+        return view('characters.characterList', ['characters' => $characters]);
     }
 
     /**
