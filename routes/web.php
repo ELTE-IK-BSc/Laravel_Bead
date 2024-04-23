3c89bd2e75c8096217772e205d04cc24a570d58b
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-23 23:22:13 +0200

    Add main route

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('main');
+})->name('main');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
