c177a6bd593f023bc2672737a0cd942981d635c6
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-24 00:17:31 +0200

    Add characters page

---
+++ resources/views/characters/characterList.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.baseLayout')
+
+@section('title', 'Karakterek')
+@section('header', 'Karakterek')
+
+@section('content')
+        <table class="table table-pin-rows">
+            <thead class="text-center">
+                <tr>
+                    <th>Név</th>
+                    <th>Védekező képességpont</th>
+                    <th>Támadó képességpont</th>
+                    <th>Pontosság képességpont</th>
+                    <th>Mágikus képességpont</th>
+                </tr>
+            </thead>
+            <tbody class="text-center">
+                @foreach ($characters as $character)
+                    <tr>
+                        <td>{{$character->name}}</td>
+                        <td>{{$character->defence}}</td>
+                        <td>{{$character->strength}}</td>
+                        <td>{{$character->accuracy}}</td>
+                        <td>{{$character->magic}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+@endsection
