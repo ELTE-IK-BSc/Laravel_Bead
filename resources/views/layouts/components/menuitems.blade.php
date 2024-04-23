924f2389b13df3dc6a8a7e6732336db1dcae7563
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-23 23:23:56 +0200

    Add menu items component

---
+++ resources/views/layouts/components/menuitems.blade.php
@@ -0,0 +1,3 @@
+<li> <a href="{{ route('main') }}" class="p-2 m-1 hover:bg-slate-100 hover:text-gray-800 hover:rounded dark:text-gray-200 dark:hover:bg-slate-900 dark:hover:text-gray-200 ">Karakterek</a></li>
+<li> <a href="{{ route('main') }}" class="p-2 m-1 hover:bg-slate-100 hover:text-gray-800 hover:rounded dark:text-gray-200 dark:hover:bg-slate-900 dark:hover:text-gray-200 ">Helyszínek</a></li>
+<li> <a href="{{ route('main') }}" class="p-2 m-1 hover:bg-slate-100 hover:text-gray-800 hover:rounded dark:text-gray-200 dark:hover:bg-slate-900 dark:hover:text-gray-200 ">Újmérkőzés</a></li>
