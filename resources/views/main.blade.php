c6c2fff989b72ed6d103454189eedb8cffd274bf
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-23 23:22:59 +0200

    Add main page

---
+++ resources/views/main.blade.php
@@ -0,0 +1,35 @@
+<?php
+use App\Models\User;
+use App\Models\Contest;
+?>
+
+
+
+@extends('layouts.baseLayout')
+
+@section('title', 'Főoldal')
+@section('header', 'Főoldal')
+
+@section('content')
+    <div class="overflow-x-auto h-96">
+        <h2>A játék leírása</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem adipisci fuga sapiente ad, facilis minus maxime
+            odio doloribus perspiciatis harum quae ab beatae impedit neque provident facere error. Quia, porro?</p>
+        <table class=" m-auto ">
+            <thead class="text-center">
+                <tr class="bg-blue-300 dark:bg-blue-900">
+                    <th class="p-1 ">Karakterek száma</th>
+                    <th class="p-1">Lejátszott mérkőzések</th>
+                </tr>
+            </thead>
+            <tbody class="text-center">
+
+                <tr class="bg-blue-100 dark:bg-blue-400">
+
+                    <td>{{ count(User::all()) }}</td>
+                    <td>{{ count(Contest::all()) }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+@endsection
