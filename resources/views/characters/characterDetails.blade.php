00393bdcdb909acdf97dcad984dd2bdf2055e0ed
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-24 02:29:36 +0200

    Create characterDetails.blade.php

---
+++ resources/views/characters/characterDetails.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.baseLayout')
+
+@section('title', 'Karakterek részletek')
+@section('header', $character->name . ' adatai')
+
+@section('content')
+    <div class="flex justify-between gap-5">
+        <table class="table table-pin-rows max-w-fit">
+            <thead class="text-center">
+                <tr>
+                    <th colspan="2">Karakter Adatok</th>
+                </tr>
+            </thead>
+            <tbody class="text-center">
+                <tr>
+                    <th>Név</th>
+                    <td>{{ $character->name }}</td>
+                </tr>
+                <tr>
+                    <th>Védekező képességpont</th>
+                    <td>{{ $character->defence }}</td>
+                </tr>
+                <tr>
+                    <th>Pontosság képességpont</th>
+                    <td>{{ $character->accuracy }}</td>
+                </tr>
+                <tr>
+                    <th>Támadó képességpont</th>
+                    <td>{{ $character->strength }}</td>
+                </tr>
+                <tr>
+                    <th>Mágikus képességpont</th>
+                    <td>{{ $character->magic }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="flex-grow">
+
+            <table class="table table-pin-rows ">
+                <thead class="text-center">
+                    <tr>
+                        <th>Helyszín</th>
+                        <th>Ellenfél</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    @foreach ($character->contests()->where('hero', '=', 1)->get() as $contest)
+                        <tr>
+                            <td>{{ $contest->place->name }}</td>
+                            <td>{{ $contest->enemy->first()->name }}</td>
+                        </tr>
+                    @endforeach
+                    @foreach ($character->contests()->where('hero', '=', 0)->get() as $contest)
+                        <tr>
+                            <td>{{ $contest->place->name }}</td>
+                            <td>{{ $contest->hero->first()->name }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+@endsection
