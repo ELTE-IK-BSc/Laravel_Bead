928b9d5fb3e73f9cd64ce7f80f5d13bdb9ca1a40
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-26 08:19:01 +0200

    Edit place item
    Add admin only
    Add places end route

--- resources/views/layouts/components/menuitems.blade.php
+++ resources/views/layouts/components/menuitems.blade.php
@@ -1,2 +1,4 @@
 <li> <a href="{{ route('characters') }}" class="p-2 m-1 hover:bg-slate-100 hover:text-gray-800 hover:rounded dark:text-gray-200 dark:hover:bg-slate-900 dark:hover:text-gray-200 ">Karakterek</a></li>
-<li> <a href="{{ route('main') }}" class="p-2 m-1 hover:bg-slate-100 hover:text-gray-800 hover:rounded dark:text-gray-200 dark:hover:bg-slate-900 dark:hover:text-gray-200 ">Helyszínek</a></li>
+@if (Auth::user()->admin)
+    <li> <a href="{{ route('places.index') }}" class="p-2 m-1 hover:bg-slate-100 hover:text-gray-800 hover:rounded dark:text-gray-200 dark:hover:bg-slate-900 dark:hover:text-gray-200 ">Helyszínek</a></li>
+@endif
