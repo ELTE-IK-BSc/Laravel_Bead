d8c779f9d942ee884b5b7c2600e6679b9674c119
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-28 22:20:17 +0200

    Add attack and round logic

--- app/Http/Controllers/ContestController.php
+++ app/Http/Controllers/ContestController.php
@@ -36,7 +36,7 @@ class ContestController extends Controller
         $character = Auth::user()->characters()->where('characters.id', $characterid)->first();
         $place = Place::all()->random(1)->first();
         $enemy = Character::all()->where('enemy', '=', 1)->random(1)->first();
-        $contest = Contest::factory()->create(['win' => null,'history' => '', 'user_id' => Auth::user()->id, 'place_id' => $place->id]);
+        $contest = Contest::factory()->create(['win' => null, 'history' => '', 'user_id' => Auth::user()->id, 'place_id' => $place->id]);
         $contest->characters()->attach($character, ['hero' => true]);
         $contest->characters()->attach($enemy, ['hero' => false]);
 
@@ -52,9 +52,8 @@ class ContestController extends Controller
         if (!$contest) {
             abort(404);
         }
-        $character = $contest->hero->first();
-        $enemy = $contest->enemy->first();
-        return view('constests.contest', ['contest' => $contest, 'character' => $character, 'enemy' => $enemy]);
+
+        return view('constests.contest', ['contest' => $contest]);
     }
 
     /**
@@ -83,5 +82,91 @@ class ContestController extends Controller
 
     public function attack(string $id, string $attackType)
     {
+        // check for contest
+        $contest = Auth::user()->contests()->where('contests.id', $id)->first();
+        if (!$contest) {
+            abort(404);
+        }
+        $charName = $contest->hero->first()->name;
+        $enemyName = $contest->enemy->first()->name;
+
+        // create character arrays for round
+        $character = [
+            'hp' => $contest->hero->first()->pivot->hero_hp,
+            'defence' => $contest->hero->first()->defence,
+            'strength' => $contest->hero->first()->strength,
+            'accuracy' => $contest->hero->first()->accuracy,
+            'magic' => $contest->hero->first()->magic,
+        ];
+        $enemy = [
+            'hp' => $contest->enemy->first()->pivot->enemy_hp,
+            'defence' => $contest->enemy->first()->defence,
+            'strength' => $contest->enemy->first()->strength,
+            'accuracy' => $contest->enemy->first()->accuracy,
+            'magic' => $contest->enemy->first()->magic,
+        ];
+
+
+        // hero attack
+        $attack = $this->calcAttack($attackType, $character, $enemy);
+
+        // update enemy
+        $enemyNewHP = $contest->enemy->first()->pivot->enemy_hp - $attack;
+        if ($enemyNewHP < 0) {
+            $enemyNewHP = 0;
+        }
+        $contest->enemy->first()->pivot->update(['enemy_hp' => $enemyNewHP]);
+        // update history
+        $change = $contest->history . "{$charName}: {$attackType} attack - {$attack} demage";
+        $contest->update(['history' => $change]);
+        // check for end
+        if ($contest->enemy->first()->pivot->enemy_hp == 0) {
+            $contest->update(['win' => true]);
+            return view('constests.contest', ['contest' => $contest]);
+        }
+
+        // random selection
+        $attacktypes = array("melee", "ranged", "special");
+        $attackType = $attacktypes[array_rand($attacktypes)];
+
+        // enemy attack
+        $attack = $this->calcAttack($attackType, $character, $enemy);
+
+        // update hero
+        $heroNewHP = $contest->hero->first()->pivot->hero_hp - $attack;
+        if ($heroNewHP < 0) {
+            $heroNewHP = 0;
+        }
+        $contest->hero->first()->pivot->update(['hero_hp' => $heroNewHP]);
+        // update history
+        $change = $contest->history . "{$enemyName}: {$attackType} attack - {$attack} demage";
+        $contest->update(['history' => $change]);
+        // check for end
+        if ($contest->hero->first()->pivot->hero_hp == 0) {
+            $contest->update(['win' => false]);
+            return view('constests.contest', ['contest' => $contest]);
+        }
+
+        // return to contest page
+
+        return view('constests.contest', ['contest' => $contest]);
+    }
+
+    private function calcAttack(string $attacktype, array $ATT, array $DEF)
+    {
+        $attack = 0;
+        if ($attacktype == "melee") {
+            $attack = ($DEF['hp']) - (($ATT['strength'] * 0.7 + $ATT['accuracy'] * 0.1 + $ATT['magic'] * 0.1) - $DEF['defence']);
+        } elseif ($attacktype == "ranged") {
+            $attack = ($DEF['hp']) - (($ATT['strength'] * 0.1 + $ATT['accuracy'] * 0.7 + $ATT['magic'] * 0.1) - $DEF['defence']);
+        } elseif ($attacktype == "special") {
+            $attack = ($DEF['hp']) - (($ATT['strength'] * 0.1 + $ATT['accuracy'] * 0.1 + $ATT['magic'] * 0.7) - $DEF['defence']);
+        }
+
+        if ($attack > 0) {
+            return $attack;
+        } else {
+            return 0;
+        }
     }
 }
