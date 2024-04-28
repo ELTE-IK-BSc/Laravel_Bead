ba3b2387ddc42383f1558649e78c3bee4bbb0274
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-28 20:37:25 +0200

    Add default images

--- database/factories/PlaceFactory.php
+++ database/factories/PlaceFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+require_once 'vendor/autoload.php';
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Place>
  */
@@ -16,11 +18,15 @@ class PlaceFactory extends Factory
      */
     public function definition(): array
     {
-        $file = 'placeholder.png';
+        $num = fake()->numberBetween(1, 3);
+        $imagename = "RandomCity".$num.".jpg";
+
+        $image = fake()->image(null, 360, 360, 'animals', true, true, 'cats', true, 'jpg');
+
         return [
             'name' => fake()->city(),
-            'imagename' => $file,
-            'imagename_hash' => $file,
+            'imagename' => $imagename,
+            'imagename_hash' => $imagename,
         ];
     }
 }
